448185f0233ac175d72a41bb5a4e8b8688ec700b
Bryce Thuilot <bryce@millcreekagency.com>
2018-07-30 14:21:47 -0400

    Need to update in QQ

--- Commission-Parser.rb
+++ Commission-Parser.rb
@@ -1,14 +1,16 @@
 #!/usr/local/bin/env ruby
-
+require 'io/console'
 require 'rubyXL'
 require 'selenium-webdriver'
 
+
+USER_EMAIL='dean@millcreekagency.com'
 
 class CommissionParser
 
   
   def initialize
-    workbook = RubyXL::Parser.parse("MILL_CREEK.xlsx")
+    workbook = RubyXL::Parser.parse("MILL CREEK (10).xlsx")
     @worksheet = workbook.worksheets[0] 
     @start_index = self.find_start(@worksheet)
   end
@@ -56,7 +58,9 @@ class CommissionParser
         #puts self.get_commission_value(row_sheet[7])
         previous_insured[:commission] += self.get_commission_value(row_sheet[7])
       else
-        @policies.push(previous_insured)
+        if previous_insured != {}
+          @policies.push(previous_insured)
+        end
         previous_insured = {
           policy_num: self.get_cell_value(row_sheet[2]),
           name: self.get_cell_value(row_sheet[1]),
@@ -105,12 +109,20 @@ class CommissionParser
     end
     return true
   end
+
+  def update
+    web_driver = WebDriver.new
+
+    @policies.each do |policy|
+      web_driver.update_policy policy[:policy_num], policy[:name], policy[:commission], policy[:date], policy[:company]
+    end
+  end
 end
 
 
 class WebDriver
 
-  def initalize
+  def initialize
     @driver = Selenium::WebDriver.for :chrome
     @driver.navigate.to 'https://app.qqcatalyst.com/Contacts/MGA/Details/7152'
     print 'Please enter password:'
@@ -119,48 +131,118 @@ class WebDriver
 
     if @driver.current_url.include? 'login.qqcatalyst.com'
       # Logs in
-      emailField = driver.find_element(name: 'txtEmail')
-      emailField.send_keys 'tory@millcreekagency.com'
-      passField = driver.find_element(id: 'txtPassword')
+      emailField = @driver.find_element(name: 'txtEmail')
+      emailField.send_keys USER_EMAIL
+      passField = @driver.find_element(id: 'txtPassword')
       passField.send_keys pass
       @driver.find_element(id: 'lnkSubmit').click
 
       sleep(1)
 
       if @driver.current_url.include? 'login.qqcatalyst.com'
-        yes_button = driver.find_element(id: 'lnkCancel')
+        yes_button = @driver.find_element(id: 'lnkCancel')
         yes_button.click
       end
     end
 
+    sleep(1)
+
     # Go to reconcile
-    reconcile_button = @driver.find_elements(class: "ReconileContact")[0]
+    reconcile_button = @driver.find_elements(class: "ReconcileContact")[0]
     reconcile_button.click
 
-    until @driver.current_url.include? 'ReconcileWorkflow?'
+
+    until @driver.current_url.include? 'ReconcileWorkFlow'
       sleep(1)
     end
 
+    sleep(1)
     @driver.find_element(id: "CarrierMGA_Paying_Agency_Commissions").click
+    
+    # Click next button
+    buttons = @driver.find_elements(class: "basic-page-next")
+    buttons.each do |button|
+      if (button.attribute("innerHTML").include? "Next") && button.displayed?
+        button.click
+      end
+    end
 
-    until @driver.current_url.include? 'ReconcileSelectPoilices'
+    until @driver.current_url.include? 'ReconcileWorkflow'
       sleep(1)
     end
   end
 
   def update_policy policy_num, name, commission, date, company 
+    sleep(1)
+    search_policy = @driver.find_element(name: "sch-policyNumber")
+    search_name = @driver.find_element(name: "sch-insured")
+    search_policy.clear
+    search_name.clear
     if policy_num != nil
-      @driver.find_element(name: "sch-policyNumber").send_keys policy_num
+      search_policy.send_keys policy_num
     else
-      @driver.find_element(name: "sch-insured").send_keys name
+      search_name.send_keys name
     end
+
+    button = find_policy_button @driver.find_elements(class: "ResetTutorialsButton")
+    button.click
+
+    sleep(2)
     
+    if @driver.find_elements(class: "simplemodal-wrap")[0] != nil
+      buttons = @driver.find_elements(class: "close")
+      buttons.each do |close_button|
+        if (close_button.attribute("innerHTML").include? "OK") && close_button.displayed?
+          close_button.click
+        end
+      end
+      return
+    end
+
+    # Get search results
+    search_results = @driver.find_elements(tag_name: "tbody")
+    search_results.each do |result|
+      date_title = false
+      policy_num_title = false
+      result.find_elements(tag_name: "td").each do |cell|
+        title = cell.property("title")
+        if policy_num != nil
+          if (title.include? policy_num)
+            policy_num_title = true
+          end
+        else 
+          if (title.include? name.split(" ")[0])
+            polciy_num_title = true
+          end
+        end
+        if (title.include? format_date(date))
+          date_title = true
+        end
+      end
+      if date_title and policy_num_title
+        result.click
+      end
+    end
+
+  end
+
+  def format_date date
+    return date.strftime("%m/%d/%Y")
+  end
+
+  def find_policy_button elements
+    elements.each do |element|
+      if (element.attribute("innerHTML").include? "Find Policies") && element.displayed?
+        return element
+      end
     end
   end
+
+  
 
 
 end
 
 cp = CommissionParser.new
 cp.find_insureds
-cp.display_policies
+cp.update
